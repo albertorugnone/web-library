4b6f3b97637f9928e75553b5d15705c95108542f
Alberto Rugnone <rugnonealberto@gmail.com>
2015-07-19 23:19:10 +0200

    added Author resource

--- web-library-be/src/main/java/it/ar/gc/library/services/AuthorsResources.java
+++ web-library-be/src/main/java/it/ar/gc/library/services/AuthorsResources.java
@@ -1,35 +1,51 @@
 package it.ar.gc.library.services;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/authors")
 public class AuthorsResources {
 
-  private final Set<Author> authors;
+  private final Map<String, Author> authors;
 
   /**
    *
    */
   public AuthorsResources() {
-    this.authors = new HashSet<Author>();
-    this.authors.add(AuthorBuilder.author().withAge("23").withId("12312")
-        .withName("Massimo Guilizzoni").withNickname("Peldi").build());
-    this.authors.add(AuthorBuilder.author().withAge("49").withId("231232")
-      .withName("Jurie Kravaciow").withNickname("Jurie").build());
-    this.authors.add(AuthorBuilder.author().withAge("28").withId("3312").withName("Samule Guidoli")
-        .withNickname("Samu28").build());
+    this.authors = new HashMap<String, Author>();
+    this.authors.put("12312",
+      AuthorBuilder.author().withAge("23").withId("12312").withName("Massimo Guilizzoni")
+      .withNickname("Peldi").build());
+    this.authors.put("231232",
+      AuthorBuilder.author().withAge("49").withId("231232").withName("Jurie Kravaciow")
+      .withNickname("Jurie").build());
+    this.authors.put("3312",
+      AuthorBuilder.author().withAge("28").withId("3312").withName("Samule Guidoli")
+      .withNickname("Samu28").build());
+  }
+
+  @GET()
+  @Path("{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Author getAuthor(@PathParam("id") String id) {
+    if (!this.authors.containsKey(id)) {
+      throw new NotFoundException();
+    }
+    return this.authors.get(id);
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public Set<Author> getAuthors() {
-    return this.authors;
+  public Collection<Author> getAuthors() {
+    return this.authors.values();
   }
 
 }
